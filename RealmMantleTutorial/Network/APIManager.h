2615fcfcedb16349a321340d5a060191aa342b83
gregoryvit <gregoryvit@gmail.com>
2015-08-06 21:01:58 +0600

    Added APIManager class

---
+++ RealmMantleTutorial/Network/APIManager.h
@@ -0,0 +1,13 @@
+//
+//  APIManager.h
+//  RealmMantleTutorial
+//
+//  Created by Gregory Berngardt on 06/08/15.
+//  Copyright (c) 2015 Gregory Berngardt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APIManager : SessionManager
+
+@end
---
+++ RealmMantleTutorial/Network/APIManager.m
@@ -0,0 +1,13 @@
+//
+//  APIManager.m
+//  RealmMantleTutorial
+//
+//  Created by Gregory Berngardt on 06/08/15.
+//  Copyright (c) 2015 Gregory Berngardt. All rights reserved.
+//
+
+#import "APIManager.h"
+
+@implementation APIManager
+
+@end
